393b39647322a3c4f74329a6c5b291dc0a911b50
Antoine Lever <antoinelever@me.com>
2024-07-23 18:10:16 +0100

    Dart update

--- lua/lsp/treesitter.lua
+++ lua/lsp/treesitter.lua
@@ -1,49 +1,48 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "c",
-        "dart",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-]>",
-          node_incremental = "<C-]>",
-          scope_incremental = false,
-          node_decremental = "<C-[>",
-        },
-      },
-    })
-  end,
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"c",
+				"dart",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-]>",
+					node_incremental = "<C-]>",
+					scope_incremental = false,
+					node_decremental = "<C-[>",
+				},
+			},
+		})
+	end,
 }
-
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,42 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
---    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		--    "folke/todo-comments.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
- -- local transform_mod = require("telescope.actions.mt").transform_mod
- -- local trouble = require("trouble")
- -- local trouble_telescope = require("trouble.sources.telescope")
+		local transform_mod = require("telescope.actions.mt").transform_mod
+		local trouble = require("trouble")
+		local trouble_telescope = require("trouble.sources.telescope")
+		require("telescope").load_extension("flutter")
+		require("telescope").extensions.flutter.commands()
 
-    -- or create your custom action
-    -- local custom_actions = transform_mod({
-    --   open_trouble_qflist = function(prompt_bufnr)
-    --     trouble.toggle("quickfix")
-    --   end,
-    -- })
+		-- or create your custom action
+		-- local custom_actions = transform_mod({
+		--   open_trouble_qflist = function(prompt_bufnr)
+		--     trouble.toggle("quickfix")
+		--   end,
+		-- })
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["Up"] = actions.move_selection_previous, -- move to prev result
-            ["Down"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            -- ["<C-t>"] = trouble_telescope.open,
-          },
-        },
-      },
-    })
-    telescope.load_extension("fzf")
-  end,
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["Up"] = actions.move_selection_previous, -- move to prev result
+						["Down"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						-- ["<C-t>"] = trouble_telescope.open,
+					},
+				},
+			},
+		})
+		telescope.load_extension("fzf")
+	end,
 }
